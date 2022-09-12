ee6dec6d736c624d36e09c42286d05273d61f056
Christina Woodhams <cwoodhams89@gmail.com>
2022-09-11 21:04:25 -0400

    finished problem

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,17 @@
 // Write your code below
+
+const word = 'racecar'
+
+const palindromeChecker = (str) => {
+    const wordSplit = str.split('');
+    const wordBackwards = [];
+    for(let i = wordSplit.length; i >= 0; i--) {
+        wordBackwards.push(wordSplit[i]);
+    }
+    if(wordBackwards.join('') === str){
+        console.log(`${str} is a palindrome.`);
+    } else {
+        console.log(`${str} is not a palindrome`)
+    }
+}
+palindromeChecker(word);
